9f05913e772884c44fc968f609fa36aedac343a9
Anwesh43 <anweshthecool0@gmail.com>
2019-01-27 15:01:15 +0530

    adding libraries and constants

--- onetwentylineview/src/main/java/com/anwesh/uiprojects/onetwentylineview/OneTwentyLineView.kt
+++ onetwentylineview/src/main/java/com/anwesh/uiprojects/onetwentylineview/OneTwentyLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.onetwentylineview
 /**
  * Created by anweshmishra on 27/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
