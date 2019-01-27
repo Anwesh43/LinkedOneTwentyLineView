ee5b31390eb69f5639cb2af98643ba95d0f7a467
Anwesh43 <anweshthecool0@gmail.com>
2019-01-27 15:16:52 +0530

    adding OneTwentyLineView class

--- onetwentylineview/src/main/java/com/anwesh/uiprojects/onetwentylineview/OneTwentyLineView.kt
+++ onetwentylineview/src/main/java/com/anwesh/uiprojects/onetwentylineview/OneTwentyLineView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawOTLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class OneTwentyLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
