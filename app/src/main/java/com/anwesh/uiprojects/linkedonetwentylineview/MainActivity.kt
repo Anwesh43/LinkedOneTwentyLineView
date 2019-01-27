bd25dac6a908a96d2b3aaac2c797451b1b6e319b
Anwesh43 <anweshthecool0@gmail.com>
2019-01-27 15:58:01 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedonetwentylineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedonetwentylineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedonetwentylineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.onetwentylineview.OneTwentyLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         OneTwentyLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
