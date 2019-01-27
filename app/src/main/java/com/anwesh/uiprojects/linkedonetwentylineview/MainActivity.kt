c9d8443cdd47c71ee434232b72b9bcdee1fbbd39
Anwesh43 <anweshthecool0@gmail.com>
2019-01-27 15:57:11 +0530

    adding view inactivity

--- app/src/main/java/com/anwesh/uiprojects/linkedonetwentylineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedonetwentylineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedonetwentylineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.onetwentylineview.OneTwentyLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        OneTwentyLineView.create(this)
     }
 }
